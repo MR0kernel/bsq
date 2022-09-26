3706b854c3fa592f3847f5d6296391d914818a80
MR0kernel <mr0kernel@RGuilherme.localdomain>
2022-09-26 02:54:56 +0200

    Premiere partie parseur

--- parseur.c
+++ parseur.c
@@ -1,12 +1,46 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <fcntl.h>
 
-void	file_open(char path)
+# define BUFF_SIZE 2048
+
+int	str_len(char *c)
 {
-	int file_fopen;
-	char a[1];
+	int i = 0;
+	while (c[i++])
+	return (i);
+}
+void	file_open(char *path)
+{
+	int file_fd;
+	int ret;
+	char tmp[1];
+	char *buffer;
+	char r;
+	int x;
 	int i;
 
-	i = open(file_fopen, a, 1);
-	
+	i = 0;
+	file_fd = open(path, O_RDONLY);
+	while ((ret = read(file_fd, tmp, 1))){
+		i += ret;
+	}
+	close(file_fd);
+	buffer = (char *)malloc(sizeof (char *) * i);
+		if (!buffer)
+			return ;
+	file_fd = open(path, O_RDONLY);
+	r = read(file_fd, buffer, i);
+	printf("%c",buffer[x]);
+	for (int y = 0; y < i; y++)
+		printf("%c",buffer[y]);
+
+}
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	file_open(argv[1]);
+	return(0);
 }
